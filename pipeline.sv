a367af5f52de3f8997e3b6b3116bbccc4d9d41ee
Austin Appleby <aappleby@gmail.com>
2023-12-01 16:28:47 -0800

    checkpoint

--- pipeline.sv
+++ pipeline.sv
@@ -1,63 +1,87 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 11/30/2023 10:38:09 PM
-// Design Name: 
+// Design Name:
 // Module Name: pipeline
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module pipeline
+#(
+  parameter constant = 32'h84573215
+)
 (
-    input  logic      clock,
-    input  logic      reset,
+  input  logic       clock,
+  input  logic       reset,
 
-    input  logic[7:0] data,
-    input  logic      data_valid,
-    output logic      data_ready,
+  input  logic[15:0] data,
+  input  logic       data_valid,
+  output logic       data_ready,
 
-    output logic[7:0] result,
-    output logic      result_valid,
-    input  logic      result_ready
+  output logic[15:0] result,
+  output logic       result_valid,
+  input  logic       result_ready
 );
 
-    logic[7:0] temp;
-    logic[7:0] temp_next;
-    logic      done;
+  logic[15:0] temp;
+  logic[15:0] temp_next;
+  logic      done;
 
-    initial begin
-    end
+  initial begin
+    temp = 8'b0;
+    data_ready = 0;
+    result = 0;
+    result_valid = 0;
+  end
 
-    always_comb begin
-        temp_next = temp + 7;
-        done = (temp_next[3:0] == 0);
-        data_ready = done;
-    end
+  always_comb begin
+    logic[31:0] m;
 
-    always @(posedge clock) begin
-        if (reset) begin
-            temp <= 0;
-        end else begin
-            if (done && data_valid) begin
-                temp <= data;
-            end else begin 
-                temp <= temp_next;
-            end
-        end
+    m = temp;
+    m ^= m >> 9;
+    m ^= m << 7;
+    m += 16'h1431;
+    m ^= 16'h4237;
+
+    temp_next = m;
+    done = (temp_next[1:0] == 0);
+
+  end
+
+  always @(posedge clock) begin
+    if (reset) begin
+      temp <= 0;
+    end else begin
+      result_valid <= 0;
+      data_ready <= 0;
+
+      if (done && data_valid && result_ready) begin
+
+        temp         <= data;
+        data_ready   <= 1;
+
+        result       <= temp;
+        result_valid <= 1;
+
+      end else begin
+        if (!done) temp <= temp_next;
+      end
     end
+  end
 
 
 endmodule
--- testbench.sv
+++ testbench.sv
@@ -1,58 +1,93 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 11/30/2023 10:58:49 PM
-// Design Name: 
+// Design Name:
 // Module Name: testbench
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
-
-//`include "pipeline.sv"
+`include "pipeline.sv"
 
 module testbench();
-  
-    logic      clock;
-    logic      reset;
-    logic[7:0] data;
-    logic      data_valid;
-    logic      data_ready;
-    
-    initial clock = 0;
-    always begin
-        #5 clock = ~clock;
+
+  logic       clock;
+  logic       reset;
+
+  logic[15:0] data;
+  logic       data_valid;
+  logic       data_ready;
+
+  logic[15:0] result;
+  logic       result_valid;
+  logic       result_ready;
+
+  //----------------------------------------
+
+  initial clock = 0;
+  always #5 clock = ~clock;
+
+  //----------------------------------------
+
+  initial begin
+    //data = 0;
+    //data_valid = 1;
+    result_ready = 1;
+  end
+
+  /*
+  always @(posedge clock) begin
+    if (data_valid && data_ready) begin
+      data <= data + 1;
     end
-    
-    
-    pipeline my_pipeline(.clock(clock), .reset(reset), .data(data), .data_valid(data_valid), .data_ready(data_ready));
-    
-    initial begin
-        $display("Hello World\n");
-        
-        reset = 1;
-        data = 0;
-        data_valid = 0;
-        
-        #10
-        
-        reset = 0;
-        
-        #100
-        
-        $display("Done");
-        $finish();
-    end
-    
+  end
+  */
+
+  //----------------------------------------
+
+  pipeline my_pipeline(
+    .clock(clock),
+    .reset(reset),
+    .data(data),
+    .data_valid(data_valid),
+    .data_ready(data_ready),
+    .result(result),
+    .result_valid(result_valid),
+    .result_ready(result_ready)
+  );
+
+
+  initial begin
+    $dumpfile("pinwheel.vcd");
+    $dumpvars(0, testbench);
+    $display("================================================================================");
+    $display("Hello World");
+
+    reset = 1;
+    data = 0;
+
+    #10
+
+    reset = 0;
+    data = 0;
+
+    #150
+
+    $display("Done");
+    $display("================================================================================");
+    $finish();
+  end
+
 endmodule
